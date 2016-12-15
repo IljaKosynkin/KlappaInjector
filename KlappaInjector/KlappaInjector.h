b446987a934a7c0a4cc3e7504d60c4b933a64da6
Ilja Kosynkin <iljakosynkin@Iljas-MacBook-Pro-2.local>
2016-12-15 21:35:08 +0100

    Initial Commit

---
+++ KlappaInjector/KlappaInjector.h
@@ -0,0 +1,19 @@
+//
+//  KlappaInjector.h
+//  KlappaInjector
+//
+//  Created by Ilja Kosynkin on 12/15/16.
+//  Copyright © 2016 Ilja Kosynkin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for KlappaInjector.
+FOUNDATION_EXPORT double KlappaInjectorVersionNumber;
+
+//! Project version string for KlappaInjector.
+FOUNDATION_EXPORT const unsigned char KlappaInjectorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <KlappaInjector/PublicHeader.h>
+
+
